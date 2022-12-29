a1f2f10549c82685387c2511e35e8992b6b17790
Christian Reifenscheid <dev@christian-reifenscheid.de>
2022-12-29 21:30:22 +0100

    [FEATURE] php 8.1 compatibility
    Adds compatibility for php 8.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'php' => '8.0-8.0.99',
+            'php' => '8.0-8.1.99',
             'typo3' => '11.5.0-11.5.99'
         ]
     ],
