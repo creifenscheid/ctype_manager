4694da244539f48e6431cd642a84fd8912b81f52
creifenscheid <dev@christian-reifenscheid.de>
2023-04-08 21:10:41 +0200

    [TASK] TYPO3 12
    Adds Services.php

---
+++ Configuration/Services.php
@@ -0,0 +1,14 @@
+<?php
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void {
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->private()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('CReifenscheid\\CtypeManager\\', __DIR__ . '/../Classes/');
+};
