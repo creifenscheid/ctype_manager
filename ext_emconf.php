6a597678984d7f91267db884ab9e3736c3ed8465
Christian Reifenscheid <christian.reifenscheid@599media.de>
2024-10-08 07:00:38 +0200

    [TASK] Upgrade
    Raises dependencies in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'php' => '8.2-8.3.99',
-            'typo3' => '12.4.0-13.3.99',
+            'php' => '8.1.0-8.2.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
     ],
     'autoload' => [
